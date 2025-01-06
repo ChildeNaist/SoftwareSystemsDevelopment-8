d76cc618b4884aba798ebc03139364cc454eae96
Childeric Kiswendsida Yameogo <childericy@gmail.com>
2025-01-06 14:53:22 +0900

    add github username

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -4,7 +4,7 @@ function App() {
         <Container>
             <Row className="my-4">
                 <Col>
-                    <h1 className="text-center">YAMEOGO Kiswendsida Aristide Childeric</h1>
+                    <h1 className="text-center">ChildeNaist : YAMEOGO Kiswendsida Aristide Childeric</h1>
                 </Col>
             </Row>            
             <Row>
