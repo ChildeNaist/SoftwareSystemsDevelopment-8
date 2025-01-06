00dbd28ab8bd6799df44136801b16b00e45a986d
Childeric Kiswendsida Yameogo <childericy@gmail.com>
2025-01-06 13:12:39 +0900

    add name3

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -2,7 +2,11 @@ function App() {
     const { Container, Row, Col } = ReactBootstrap;
     return (
         <Container>
-          <h1 className="text-center">YAMEOGO Kiswendsida Aristide Childeric</h1>
+            <Row>
+                <Col>
+                    <h1 className="text-center">YAMEOGO Kiswendsida Aristide Childeric</h1>
+                </Col>
+            </Row>            
             <Row>
                 <Col md={{ offset: 3, span: 6 }}>
                     <TodoListCard />
